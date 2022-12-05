fcab31559fda6809bdd4ce6b34db740fbc5e339e
Davide Modolo <davide.modolo98@gmail.com>
2022-12-05 18:19:55 +0100

    Created naif serial version

---
+++ README.md
@@ -0,0 +1,9 @@
+# HPC Project - Closest Pair of Points
+
+TODO:
+
+[ ] function for distance between two points in _n_ dimensions: $\displaystyle\text{distance}(p_a, p_b) = \sqrt{\sum_{i=0}^{\#d}(a_i - b_i)^2}$
+
+[ ] create $M$ random points
+
+[ ] $\Omicron(n^2)$ serial version
---
+++ serial_version.c
@@ -0,0 +1,68 @@
+// import random library
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+// struct for a point of n dimensions
+typedef struct {
+    int x;
+    int y;
+    int z;
+    int w;
+    int v;
+} point;
+
+// function to calculate the distance between two points of n dimensions
+double distance(point *p1, point *p2){
+    double sum = 0;
+    sum += (p1->x - p2->x)*(p1->x - p2->x);
+    sum += (p1->y - p2->y)*(p1->y - p2->y);
+    sum += (p1->z - p2->z)*(p1->z - p2->z);
+    sum += (p1->w - p2->w)*(p1->w - p2->w);
+    sum += (p1->v - p2->v)*(p1->v - p2->v);
+    return sqrt(sum);
+}
+
+int main(){
+    // create M random points
+    int M = 20;
+    point *points = (point *)malloc(M*sizeof(point));
+
+    // cicle to initialize the points randomly
+    for(int i=0; i<M; i++){
+        points[i].x = rand()%100;
+        points[i].y = rand()%100;
+        points[i].z = rand()%100;
+        points[i].w = rand()%100;
+        points[i].v = rand()%100;
+    }
+
+    // find the closest pair of points
+    double min_dist = distance(&points[0], &points[1]);
+    // save the index of the closest pair of points
+    int index1 = 0;
+    int index2 = 1;
+
+    for(int i=0; i<M; i++){
+        for(int j=i+1; j<M; j++){
+            double dist = distance(&points[i], &points[j]);
+            if(dist < min_dist){
+                index1 = i;
+                index2 = j;
+                min_dist = dist;
+            }
+        }
+    }
+
+    // print the distance and the points
+    printf("The closest pair of points is (%d, %d, %d, %d, %d) and (%d, %d, %d, %d, %d) with distance %f", points[index1].x, points[index1].y, points[index1].z, points[index1].w, points[index1].v, points[index2].x, points[index2].y, points[index2].z, points[index2].w, points[index2].v, min_dist);
+
+    // keep the terminal open
+    getchar();
+
+    // free the memory
+    free(points);
+
+    return 0;
+}
